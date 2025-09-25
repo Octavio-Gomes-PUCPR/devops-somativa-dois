46cbb6c382a8f709e902cf18804c8e7951659ee1
Octavio-Gomes-PUCPR <octavio.gomes@pucpr.edu.br>
2025-09-24 23:20:13 -0300

    Tests

---
+++ test/test.py
@@ -0,0 +1,10 @@
+from src.main import *
+from unittest.mock import patch
+
+def test_root():
+    assert root() == {"message": "Hello World"}
+
+def test_funcaoteste():
+    with patch('random.randint', return_value=12345):
+        result = funcaoteste()
+    assert result == {"teste": True, "num_aleatorio": 12345}
