4d537b1846f8c3e45d6e8bed837046d1eddadadf
Octavio-Gomes-PUCPR <octavio.gomes@pucpr.edu.br>
2025-09-24 23:35:12 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,14 +1,16 @@
 from src.main import *
 from unittest.mock import patch
 
-def test_root():
-    result = root()
-    yield result
+import pytest
+
+@pytest.mark.asyncio
+async def test_root():
+    result = await root()
     assert result == {"message": "Hello World"}
 
-def test_funcaoteste():
+@pytest.mark.asyncio
+async def test_funcaoteste():
     with patch('random.randint', return_value=12345):
-        result = funcaoteste()
-        yield result
+        result = await funcaoteste()
 
     assert result == {"teste": True, "num_aleatorio": 12345}
